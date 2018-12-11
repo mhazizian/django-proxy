293197671ea2a96c26df66b05a748e558ec83885
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2018-12-11 06:26:14 +0330

    Amend settings

--- testsettings.py
+++ testsettings.py
@@ -1,3 +1,16 @@
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    '195.201.13.227',
+    '82.102.12.207',
+    '82.102.12.207:8080',
+    '94.182.227.193',
+    '99.91.97.238',
+    'hamclassy-test',
+    'backend.hamclassy.ir'
+]
+
 SECRET_KEY = '12345'
 DATABASES = {
     'default': {
